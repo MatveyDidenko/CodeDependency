87cb21a057a9e1b890bf138ed5f62297a7b68aff
Matvey Didenko <matvey.s.didenko@gmail.com>
2021-05-30 13:05:13 -0400

    All h files are in a list, but they are written letter by letter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 
+valid_headers = ['.h', '.hh', '.hpp']
+valid_sources = ['.c', '.cc', '.cpp']
+
 def WriteToFile(string):     #this overwrites everything that is currently in the file
     f = open("pipe.dot", 'w')
     f.write(string)
@@ -7,9 +10,15 @@ def WriteToFile(string):     #this overwrites everything that is currently in th
 
 
 rootdir = 'C:/Users/matve/Documents/AmazonCppFileSystem'
+fileList = []
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         if ".git" not in str(subdir):
             print(os.path.join(subdir, file))
-            
+
+        for header in valid_headers:
+            if header in file:
+                fileList += file
+
+print(fileList)
