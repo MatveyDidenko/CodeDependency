6c4e5dbd6b2c5e6b5f87cd3ab87df796d137464e
Matvey Didenko <matvey.s.didenko@gmail.com>
2021-05-30 13:56:01 -0400

    makes dot file of all headers

--- main.py
+++ main.py
@@ -10,19 +10,31 @@ def WriteToFile(string):     #this overwrites everything that is currently in th
 
 
 rootdir = 'C:/Users/matve/Documents/AmazonCppFileSystem'
+folders = []
 headerSourceList = []
 headerNameList = []
 
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        if ".git" not in str(subdir):
+        if ".git" not in subdir:
             print(os.path.join(subdir, file))
 
-        for header in valid_headers:
-            if header in file:
-                headerSourceList.append(os.path.join(subdir, file))
-                headerNameList.append(file)
+            for header in valid_headers:
+                if header in file:
+                    headerSourceList.append(os.path.join(subdir, file))
+                    headerNameList.append(file)
+
+res = "digraph {\n\trankdir=LR\n\n\t"
+
+res += "subgraph cluster_0 {\n\t\tlabel=\"Header Files\";\n\t\t"
+
+for name in headerNameList:
+    res += name[:len(name)-2] + "; "
+
+res += "\n"
+res += "\t}\n\n"
 
 
-print(headerSourceList, headerNameList)
+res += "}"
+WriteToFile(res)
