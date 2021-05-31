df183d9a6c1fab7eea44b69e7d063c4ed7741136
Matvey Didenko <matvey.s.didenko@gmail.com>
2021-05-31 09:31:35 -0400

    cluster function + all source files

--- main.py
+++ main.py
@@ -1,40 +1,63 @@
 import os
 
-valid_headers = ['.h', '.hh', '.hpp']
-valid_sources = ['.c', '.cc', '.cpp']
+valid_headers = ['.h']
+valid_sources = ['.cpp']
+
+rootdir = 'C:/Users/matve/Documents/AmazonCppFileSystem'
+folders = []
+headerSourceList = []
+headerNameList = []
+cppSourceList = []
+cppNameList = []
+
+
 
 def WriteToFile(string):     #this overwrites everything that is currently in the file
     f = open("pipe.dot", 'w')
     f.write(string)
     f.close()
 
+def init():
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            if ".git" not in subdir:
+                print(os.path.join(subdir, file)[len(rootdir):])
 
-rootdir = 'C:/Users/matve/Documents/AmazonCppFileSystem'
-folders = []
-headerSourceList = []
-headerNameList = []
+                for h in valid_headers:
+                    if h in file:
+                        headerSourceList.append(os.path.join(subdir, file)[len(rootdir):])
+                        headerNameList.append(file[:len(file)-2]+"_h")
 
-
-for subdir, dirs, files in os.walk(rootdir):
-    for file in files:
-        if ".git" not in subdir:
-            print(os.path.join(subdir, file))
-
-            for header in valid_headers:
-                if header in file:
-                    headerSourceList.append(os.path.join(subdir, file))
-                    headerNameList.append(file)
-
-res = "digraph {\n\trankdir=LR\n\n\t"
-
-res += "subgraph cluster_0 {\n\t\tlabel=\"Header Files\";\n\t\t"
-
-for name in headerNameList:
-    res += name[:len(name)-2] + "; "
-
-res += "\n"
-res += "\t}\n\n"
+                for cpp in valid_sources:
+                    if cpp in file:
+                        cppSourceList.append(os.path.join(subdir, file)[len(rootdir):])
+                        cppNameList.append(file[:len(file)-4]+"_cpp")
 
 
-res += "}"
-WriteToFile(res)
+def create_cluster(name, fileNameList):
+    cluster = ""
+    cluster += "\tsubgraph cluster_"+name+" {\n\t\tlabel=\""+name+"\";\n\t\t"
+
+    for fileName in fileNameList:
+        cluster += fileName + "; "
+
+    cluster += "\n"
+    cluster += "\t}\n\n"
+
+    return cluster
+
+
+def main():
+    init()
+
+    res = ""
+    res += "digraph {\n\trankdir=LR\n\n"
+
+    res += create_cluster("header", headerNameList)
+    res += create_cluster("sources", cppNameList)
+
+    res += "}"
+    WriteToFile(res)
+
+
+main()
